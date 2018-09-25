fa16ebef608ea96d5071c7480391cc0e1799efe0
yechenhui <yyzych@gmail.com>
2018-09-25 23:49:34 +0800

    prepare deploy

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'http://www.shallowhole.com/v1'
-export const resourceUrl = 'http://www.shallowhole.com'
+export const baseUrl = 'https://www.shallowhole.com/v1'
+export const resourceUrl = 'https://www.shallowhole.com'
