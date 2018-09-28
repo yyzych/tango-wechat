1753ec3098a363facaa9310ddf6c62888a450148
yechenhui <yyzych@gmail.com>
2018-09-28 22:56:37 +0800

    search bug

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const appSecret = '你真NB'
 
-// export const baseUrl = 'https://www.shallowhole.com/v1'
-// export const resourceUrl = 'https://www.shallowhole.com'
+export const baseUrl = 'https://www.shallowhole.com/v1'
+export const resourceUrl = 'https://www.shallowhole.com'
 
-export const baseUrl = 'http://192.168.0.100:8080/v1'
-export const resourceUrl = 'http://192.168.0.100:8080'
+// export const baseUrl = 'http://192.168.0.100:8080/v1'
+// export const resourceUrl = 'http://192.168.0.100:8080'
