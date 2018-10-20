3bb09e8adc328d3d285fa761dae91f126d4b4dbd
yechenhui <yyzych@gmail.com>
2018-10-20 10:15:22 +0800

    删除敏感配置，信息

--- src/config.js
+++ src/config.js
@@ -1,7 +1,4 @@
-export const appSecret = '你真NB'
+export const appSecret = ''
 
-export const baseUrl = 'https://www.shallowhole.com:10440/v1'
-export const resourceUrl = 'https://www.shallowhole.com:10440'
-
-// export const baseUrl = 'http://192.168.0.100:8080/v1'
-// export const resourceUrl = 'http://192.168.0.100:8080'
+export const baseUrl = ''
+export const resourceUrl = ''
